fb1b826b0730194055072d24a72a20df3e97d804
Alastair Munro <alastair@alastair-munro.com>
2018-12-16 00:04:47 +0000

    * Got everything pretty much working. State present/absent, reporting on changes, excluding start_time, end_time, min and max from the playbook definition, etc.

--- library/ec2_asg_scheduled_action.py
+++ library/ec2_asg_scheduled_action.py
@@ -46,7 +46,7 @@ EXAMPLES = '''
   ec2_asg_scheduled_action:
     autoscaling_group_name: test_asg
     scheduled_action_name: mtest_asg_schedule
-    start_time: 2017-06-01T10:00:00Z
+    start_time: 2017 August 18 08:00 UTC+10
     end_time: 2018 August 18 08:00 UTC+10
     recurrence: 40 22 * * 1-5
     min_size: 0
@@ -74,31 +74,46 @@ except ImportError:
 from dateutil.tz import tzutc
 import datetime
 
-import q
-
-
 def format_request(module):
     request = dict()
     request['AutoScalingGroupName'] = module.params.get('autoscaling_group_name')
     request['ScheduledActionName'] = module.params.get('scheduled_action_name')
-    request['StartTime'] = module.params.get('start_time')
-    request['EndTime'] = module.params.get('end_time')
     request['Recurrence'] = module.params.get('recurrence')
-    request['MinSize'] = module.params.get('min_size')
-    request['MaxSize'] = module.params.get('max_size')
     request['DesiredCapacity'] = module.params.get('desired_capacity')
+
+    if module.params.get('min_size') != None:
+      request['MinSize'] = module.params.get('min_size')
+
+    if module.params.get('max_size') != None:
+      request['MaxSize'] = module.params.get('max_size')
+
+    if module.params.get('start_time') != None:
+      request['StartTime'] = module.params.get('start_time')
+
+    if module.params.get('end_time') != None:
+      request['EndTime'] = module.params.get('end_time')
+
     return request
 
 
 def delete_scheduled_action(client, module):
+    changed = False
+    actions = describe_scheduled_actions(client, module)
+    xx = actions.get("ScheduledUpdateGroupActions")
+
+    if len(xx) == 0:
+      return changed, actions
+
+    changed = True
     params = dict()
     params['AutoScalingGroupName'] = module.params.get('autoscaling_group_name')
     params['ScheduledActionName'] = module.params.get('scheduled_action_name')
     try:
-        pass
-        # return client.delete_scheduled_action(**params)
+        actions = client.delete_scheduled_action(**params)
     except botocore.exceptions.ClientError as e:
         module.fail_json(msg=str(e))
+
+    return changed, actions
 
 
 def describe_scheduled_actions(client, module):
@@ -108,23 +123,47 @@ def describe_scheduled_actions(client, module):
             AutoScalingGroupName=module.params.get('autoscaling_group_name'),
             ScheduledActionNames=[module.params.get('scheduled_action_name')]
         )
-        q(actions['ScheduledUpdateGroupActions'][0])
     except botocore.exceptions.ClientError as e:
         pass
     return actions
 
+# The boto module requires a start and end time, and the deploy of the put sets the time it was created.
+# Thus we can't really check on the times, and must pull these off the compare for changes.
+# Also the startTime needs to be now or in the future, so that is unlikely to be specified in the playbook
+# as aws does not allow it to be in the past! So most likely only the schedule will be specified in the playbook.
+# Otherwise we need to do a fairly complex diff on what we would put against what is already there.
+# So the change on startTime and EndTime is ignored and only other settings are compared.
 
 def put_scheduled_update_group_action(client, module):
     changed = False
     params = format_request(module)
-    q(params)
-    q("                       ")
+
     exists = describe_scheduled_actions(client, module)
+    xx = exists.get("ScheduledUpdateGroupActions")
 
     try:
-        return changed, client.put_scheduled_update_group_action(**params)
+      status = client.put_scheduled_update_group_action(**params)
     except botocore.exceptions.ClientError as e:
-        module.fail_json(msg=str(e))
+      module.fail_json(msg=str(e))
+
+    if len(xx) == 0:
+      changed = True
+    else:
+      xx = xx[0]
+      exists = describe_scheduled_actions(client, module)
+      yy = exists.get("ScheduledUpdateGroupActions")[0]
+
+      for x in ['StartTime','EndTime','Time']:
+        if x in xx:
+           xx.pop(x)
+
+        if x in yy:
+           yy.pop(x)
+
+      if xx != yy:
+        changed = True
+
+    return changed, status
 
 
 def main():
@@ -143,14 +182,13 @@ def main():
     )
     module = AnsibleModule(argument_spec=argument_spec)
     state = module.params.get('state').lower()
-    q(state)
 
     if not HAS_BOTO3:
         module.fail_json(msg='json and boto3 are required.')
 
     try:
         region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
-        client = boto3_conn(module, conn_type='client', resource='autoscaling', region=region, endpoint=ec2_url, **aws_connect_kwargs)       
+        client = boto3_conn(module, conn_type='client', resource='autoscaling', region=region, endpoint=ec2_url, **aws_connect_kwargs)
     except botocore.exceptions.NoCredentialsError, e:
         module.fail_json(msg="Can't authorize connection - " + str(e))
 
@@ -158,7 +196,8 @@ def main():
         (changed, results) = put_scheduled_update_group_action(client, module)
         module.exit_json(changed=changed, results=results)
     else:
-        (changed, results) = delete_scheduled_action(state, client, module)
+        #(changed, results) = delete_scheduled_action(state, client, module)
+        (changed, results) = delete_scheduled_action(client, module)
         module.exit_json(changed=changed, results=results)
 
 
